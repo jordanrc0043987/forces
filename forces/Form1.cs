a1d2b684f4df72e8def6b0e1d694826a434a74d6
Jordan Cooper <RC0043987@student.riversidecollege.ac.uk>
2021-05-12 10:18:08 +0100

    jdghgdh

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -15,32 +15,33 @@ namespace forces
         public Form1()
         {
             InitializeComponent();
-            double sin(double x)
-            {
-                return (Math.Sin(x * Math.PI / 180.0)); 
+           
             }
-            double asin(double x)
-            {
-                return (Math.Asin(x * Math.PI / 180.0));
-            }
-            double tan(double x)
-            {
-                return (Math.Tan(x * Math.PI / 180.0));
-            }
-            double atan(double x)
-            {
-                return (Math.Atan(x * Math.PI / 180.0));
-            }
-            double cos(double x)
-            {
-                return (Math.Cos(x * Math.PI / 180.0));
-            }
-            double acos(double x)
-            {
-                return (Math.Acos(x * Math.PI / 180.0));
-            }
+        
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
         }
-
+        double asin(double x)
+        {
+            return (Math.Asin(x * Math.PI / 180.0));
+        }
+        double tan(double x)
+        {
+            return (Math.Tan(x * Math.PI / 180.0));
+        }
+        double atan(double x)
+        {
+            return (Math.Atan(x * Math.PI / 180.0));
+        }
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x * Math.PI / 180.0));
+        }
         private void PictureBox1_Click(object sender, EventArgs e)
         {
 
@@ -58,12 +59,12 @@ namespace forces
                 double Fy = Force * sin(Angle);
                 //Rewrites the answers into the lablel
                 label1.Text = "Fx = " + Fx;
-                label2.Text = "Fx = " = Fy;
+                label2.Text = "Fy = " + Fy;
             }
             catch
             {
-                //shows a message if an error occurs
-                MessageBox.Show("Your numbers are wrong, check again")
+            //shows a message if an error occurs
+            MessageBox.Show("Your numbers are wrong, check again");
             }
 
         }
